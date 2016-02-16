0fbd2db1317fe61d4ca15480ed0622561e22b961
Alex Luk <a.lukyanchanka@gmail.com>
2016-02-16 07:15:00 +0300

    0 начат нулевой этап работы над змейкой

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
+
+            int x2 = 2;
+            int y2 = 2;
+            char sym2 = '#';
+            Draw(x2, y2, sym2);
+
+
+
+           
+
+
+        }
+        static void Draw(int x,int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
