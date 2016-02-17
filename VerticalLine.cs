9c85acae095d5b3c1b402f2ad55f00b2bc2be5fd
Alex Luk <a.lukyanchanka@gmail.com>
2016-02-17 06:34:55 +0300

    vertical and horizontal lines done

---
+++ VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int x, int yBottom, int yTop, char sym) 
+        {
+            pList = new List<Point>();
+            for(int y = yBottom; y <= yTop; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+        public void Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Drow();
+            }
+        }
+    }
+}
